03f7e664432a5b2c59d4bcd25604f85b44991461
zihong <mzUafXdlDkR%>
2019-06-06 14:05:09 +0800

    测试自定义starter

--- SpringBoot-Builder-Docker/src/main/java/com/dashuai/learning/docker/DockerApplication.java
+++ SpringBoot-Builder-Docker/src/main/java/com/dashuai/learning/docker/DockerApplication.java
@@ -1,7 +1,10 @@
 package com.dashuai.learning.docker;
 
+import com.dashuai.learning.autostarter.service.ExampleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,16 @@ import java.util.Date;
 @RestController
 public class DockerApplication {
 
+    @Autowired
+    ExampleService exampleService;
     @RequestMapping("/")
     public String getDate(){
         return new Date().toString();
+    }
+
+    @RequestMapping("/auto/{name}")
+    public String getWap(@PathVariable String name){
+        return exampleService.wrap(name);
     }
 
     public static void main(String[] args) {
